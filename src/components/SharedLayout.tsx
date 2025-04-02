a44c829a0b959138faa866c04109539cc3f84a09
MartinJindu <martinchijindu@gmail.com>
2025-04-02 13:42:43 +0100

    Navbar and hero section redesign

--- src/components/SharedLayout.tsx
+++ src/components/SharedLayout.tsx
@@ -1,12 +1,13 @@
-import Navbar from "./Navbar";
 import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
+import NavbarApp from "./Navbar";
 
 const SharedLayout = () => {
   return (
     <div className="flex flex-grow flex-col font-mono min-h-screen bg-gray-900 text-white">
-      <Navbar />
-      <main className=" mx-auto max-w-[1200px] px-4 flex items-center justify-center">
+      <NavbarApp />
+
+      <main className=" mx-auto max-w-[1200px] px-4 flex items-center justify-center md:pt-24">
         <Outlet />
       </main>
       <Footer />
